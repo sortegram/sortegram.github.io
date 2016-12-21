af733db29d4227e3f0e1d16de3b3b4dadb170312
Victor Ribeiro <victor_ribeiro135@hotmail.com>
2016-12-21 16:37:04 -0200

    Update sg.sortegram.js

--- js/sg.sortegram.js
+++ js/sg.sortegram.js
@@ -2434,6 +2434,28 @@ function sg_Start ()
                     $( '#trophy' )
                       .show()
                         .addClass( 'animated infinite pulse sg_Config-winIcon-' + localStorage.getItem('winner-icon') );
+          
+          
+          
+          ////////////////////////////////////
+          if ( sgResult.match( '@' ) )
+          {
+                var str = sgResult;
+                var res = str.replace("@", "");
+              
+                $.ajax({
+                  dataType: "json",
+                  url: 'https://www.instagram.com/' + res + '/?__a=1',
+                  data: data,
+                  success: function (data) {
+                    $( '#trophy' ).css({
+                        'background-image':'url(' + data.user.profile_pic_url + ')'
+                    });
+                      
+                    console.log(data.user.profile_pic_url);
+                  }
+                });
+          }
 
                     // Esconde o loader
                     $( '#spinner' ).fadeOut();
